c0ef4e096a62ed6ef944c0bcd5fc078cdaf0bdf1
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2026-02-25 22:23:09 +0100

    Start implementing Rust backend module initialization and `new` function in Lua

--- src/lib.rs
+++ src/lib.rs
@@ -1,77 +1,68 @@
 mod backend;
 mod file_changes_monitor;
-mod script_paths;
 mod indexes;
 mod instrument_registry;
+mod script_paths;
 
+use crate::backend::Backend;
 use std::ffi::CStr;
-use std::fs;
 use std::os::raw::{c_char, c_int};
-use std::time::SystemTime;
+use std::ptr::{null, null_mut};
+use std::time::Duration;
 
 #[repr(C)]
+#[allow(non_camel_case_types)]
 pub struct lua_State {
     _private: [u8; 0],
 }
 
+#[allow(non_camel_case_types)]
+#[allow(non_snake_case)]
+type lua_CFunction = *const unsafe extern "C" fn(L: *mut lua_State) -> c_int;
+
+#[repr(C)]
+#[allow(non_camel_case_types)]
+pub struct luaL_Reg {
+    name: *const c_char,
+    func: lua_CFunction,
+}
+
+#[allow(non_snake_case)]
 unsafe extern "C" {
     fn lua_pushinteger(L: *mut lua_State, n: i64);
     fn lua_pushnil(L: *mut lua_State);
     fn lua_tolstring(L: *mut lua_State, index: c_int, len: *mut usize) -> *const c_char;
     fn lua_settable(L: *mut lua_State, index: c_int);
-    fn lua_pushcclosure(L: *mut lua_State, f: unsafe extern "C" fn(*mut lua_State) -> c_int, n: c_int);
+    fn lua_pushcclosure(
+        L: *mut lua_State,
+        f: unsafe extern "C" fn(*mut lua_State) -> c_int,
+        n: c_int,
+    );
     fn lua_pushstring(L: *mut lua_State, s: *const c_char) -> *const c_char;
     fn lua_createtable(L: *mut lua_State, narr: c_int, nrec: c_int);
+    fn luaL_checklstring(L: *mut lua_State, arg: c_int, l: *mut usize) -> *const c_char;
+    fn luaL_register(L: *mut lua_State, libname: *const c_char, l: *const luaL_Reg);
 }
 
-unsafe extern "C" fn get_file_mtime(L: *mut lua_State) -> c_int {
-    unsafe {
-        // Get the string argument from Lua stack
-        let c_str = lua_tolstring(L, 1, std::ptr::null_mut());
-        if c_str.is_null() {
-            lua_pushnil(L);
-            return 1;
-        }
+#[allow(non_snake_case)]
+unsafe extern "C" fn new(L: *mut lua_State) -> c_int {
+    let c_song_path: *const c_char = luaL_checklstring(L, 1, null_mut());
 
-        let path = match CStr::from_ptr(c_str).to_str() {
-            Ok(s) => s,
-            Err(_) => {
-                lua_pushnil(L);
-                return 1;
-            }
-        };
+    let path =
+        unsafe { CStr::from_ptr(c_song_path) }.to_str().expect("invalid UTF-8 in path").to_owned();
+    let backend = Backend::new(path, Duration::from_secs(5));
 
-        // Get file modification time
-        match fs::metadata(path) {
-            Ok(metadata) => match metadata.modified() {
-                Ok(time) => match time.duration_since(SystemTime::UNIX_EPOCH) {
-                    Ok(duration) => {
-                        lua_pushinteger(L, duration.as_secs() as i64);
-                        return 1;
-                    }
-                    Err(_) => {}
-                },
-                Err(_) => {}
-            },
-            Err(_) => {}
-        }
-
-        lua_pushnil(L);
-        1
-    }
+    1
 }
+
+const RUST_BACKEND: [luaL_Reg; 2] = [
+    luaL_Reg { name: b"new\0".as_ptr() as *const c_char, func: new as lua_CFunction },
+    luaL_Reg { name: null(), func: null() },
+];
 
 #[unsafe(no_mangle)]
+#[allow(non_snake_case)]
 pub unsafe extern "C" fn luaopen_phrase_scripts_reloader(L: *mut lua_State) -> c_int {
-    unsafe {
-        // Create a new table for our module
-        lua_createtable(L, 0, 1);
-
-        // Add get_mtime function to the table
-        lua_pushstring(L, b"get_mtime\0".as_ptr() as *const c_char);
-        lua_pushcclosure(L, get_file_mtime, 0);
-        lua_settable(L, -3);
-
-        1  // Return 1 value (the table)
-    }
+    luaL_register(L, b"rust_backend\0".as_ptr() as *const c_char, RUST_BACKEND.as_ptr());
+    1 // Return 1 value (the table)
 }
