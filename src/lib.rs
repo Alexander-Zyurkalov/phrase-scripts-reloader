3a88f8b12d95275f2b1f14e004e385b5e9aeb8f4
Alexandr Zyurkalov <Alexander.Zyurkalov@gmail.com>
2026-02-25 22:43:33 +0100

    Extend `new` function in Lua to handle UTF-8 validation and accept duration as an argument

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ mod instrument_registry;
 mod script_paths;
 
 use crate::backend::Backend;
-use std::ffi::CStr;
+use std::ffi::{c_longlong, CStr};
 use std::os::raw::{c_char, c_int};
 use std::ptr::{null, null_mut};
 use std::time::Duration;
@@ -40,18 +40,31 @@ unsafe extern "C" {
     );
     fn lua_pushstring(L: *mut lua_State, s: *const c_char) -> *const c_char;
     fn lua_createtable(L: *mut lua_State, narr: c_int, nrec: c_int);
+    fn lua_newuserdata(L: *mut lua_State, size: usize);
     fn luaL_checklstring(L: *mut lua_State, arg: c_int, l: *mut usize) -> *const c_char;
+    fn luaL_checkinteger(L: *mut lua_State, arg: c_int) -> c_longlong;
     fn luaL_register(L: *mut lua_State, libname: *const c_char, l: *const luaL_Reg);
+    fn luaL_error(L: *mut lua_State, fmt: *const c_char, ...) -> c_int;
 }
 
 #[allow(non_snake_case)]
 unsafe extern "C" fn new(L: *mut lua_State) -> c_int {
-    let c_song_path: *const c_char = luaL_checklstring(L, 1, null_mut());
+    unsafe {
+        let c_song_path: *const c_char = luaL_checklstring(L, 1, null_mut());
 
-    let path =
-        unsafe { CStr::from_ptr(c_song_path) }.to_str().expect("invalid UTF-8 in path").to_owned();
-    let backend = Backend::new(path, Duration::from_secs(5));
-
+        let path = match CStr::from_ptr(c_song_path).to_str() {
+            Ok(str) => str.to_owned(),
+            Err(err_text) => {
+                return luaL_error(
+                    L,
+                    b"Invalid UTF-8 in path: %s\0".as_ptr() as *const c_char,
+                    err_text,
+                );
+            }
+        };
+        let c_seconds = luaL_checkinteger(L, 2);
+        let backend = Backend::new(path, Duration::from_secs(c_seconds as u64));
+    }
     1
 }
 
